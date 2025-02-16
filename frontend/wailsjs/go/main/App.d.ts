db090fe70cd620145af5cb48d16b4ca41c144c48
Luiz Neves <luizgustavobenicio@gmail.com>
2025-02-16 19:27:49 -0300

    Atualiza definições de funções para retornar tipos corretos e atualiza dependências do Go

--- frontend/wailsjs/go/main/App.d.ts
+++ frontend/wailsjs/go/main/App.d.ts
@@ -12,7 +12,7 @@ export function DeleteQuery(arg1:number):Promise<void>;
 
 export function ExportDatabaseFile():Promise<void>;
 
-export function GetBuildParams():Promise<{[key: string]: any}>;
+export function GetBuildParams():Promise<Record<string, any>>;
 
 export function GetDatabaseConnection():Promise<main.DatabaseConnection>;
 
@@ -22,12 +22,12 @@ export function ImportDatabaseFile():Promise<void>;
 
 export function InsertQueryInDatabase(arg1:main.Query):Promise<void>;
 
-export function MakeBindedSQL(arg1:string,arg2:Array<{[key: string]: any}>,arg3:Array<main.Variable>):Promise<string>;
+export function MakeBindedSQL(arg1:string,arg2:Array<Record<string, any>>,arg3:Array<main.Variable>):Promise<string>;
 
 export function ReadXLSXFile():Promise<string>;
 
 export function TestDatabaseConnection(arg1:main.DatabaseConnection):Promise<boolean>;
 
-export function TestQueryInDatabase(arg1:main.DatabaseConnection,arg2:string):Promise<Array<{[key: string]: any}>>;
+export function TestQueryInDatabase(arg1:main.DatabaseConnection,arg2:string):Promise<Array<Record<string, any>>>;
 
 export function UpdateQuery(arg1:number,arg2:main.Query):Promise<void>;
